6e23bf3a2e49914260b5f6437004d4fb1a000bae
mihkriss <74809078+mihkriss@users.noreply.github.com>
2022-07-07 17:44:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
 
+# import shutil
+# import startup.py
+
 
 class Window(QWidget):
     def __init__(self):
@@ -14,228 +17,260 @@ class Window(QWidget):
         self.setWindowTitle("Media Player")
         self.setGeometry(350, 100, 700, 500)
         self.setWindowIcon(QIcon('png-clipart-computer-icons-video-the-noun-project-angle-white.png'))
-        p =self.palette()
-        p.setColor(QPalette.Window, Qt.white)
-        self.setPalette(p)
+
+        self.setStyleSheet("background-color: white;")
 
         self.init_ui()
 
-
         self.show()
 
-
     def init_ui(self):
+        self.filenameOpened = ''
+        self.filenameFolder = ''
+        self.filenameMask = ''
+        self.filenameSaved = ''
 
-        #create media player object
-        self.mediaPlayer1 = QMediaPlayer(None, QMediaPlayer.VideoSurface)
-        self.mediaPlayer2 = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        # create media player object
+        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
 
-        #create videowidget object
+        # create videowidget object
 
-        videowidget1 = QVideoWidget()
-        videowidget2 = QVideoWidget()
+        videowidget = QVideoWidget()
 
-        #create open button
-        zn = QIcon()
-        zn.addPixmap(QPixmap("png-clipart-computer-icons-video-angle-rectangle.png"))
-        self.openBtn1 = QToolButton()
-        self.openBtn1.setIcon(zn)
-        self.openBtn1.clicked.connect(lambda: self.open_file(self.mediaPlayer1, self.playBtn1))
-        #openBtn2 = QPushButton('Open Video')
-        #openBtn2.clicked.connect(lambda: self.open_file(self.mediaPlayer2, self.playBtn2))
+        # create button for playing
+
+        self.playBtn = QToolButton()
+        self.playBtn.setEnabled(False)
+        self.playBtn.setMinimumSize(60, 60)
+        self.playBtn.setStyleSheet(
+            "QToolButton {background-color: white; color: black; border-radius: 30px;  border: 2px groove gray;font: 9pt 'AcadEref';border-style: circle;}" "QToolButton:pressed {background-color:rgb(119, 136, 153) ; }")
+        self.playBtn.setIconSize(self.playBtn.minimumSize())
+        self.playBtn.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+        self.playBtn.clicked.connect(self.play_video)
+
+        # create slider
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setStyleSheet("""
+            QSlider{
+                background: white;
+            }
+            QSlider::groove:horizontal {  
+                height: 10px;
+                margin: 0px;
+                border-radius: 5px;
+                background: #D3D3D3;
+            }
+            QSlider::handle:horizontal {
+                background: #81848A;
+                border: 1px solid #6D6F73;
+                width: 17px;
+                margin: -5px 0; 
+                border-radius: 8px;
+            }
+            QSlider::sub-page:qlineargradient {
+                background: rgb(119, 136, 153);
+                border-radius: 5px;
+            }
+        """)
+        self.slider.setRange(0, 0)
+        self.slider.sliderMoved.connect(self.set_position)
+
+        # create label
+        self.label = QLabel()
+        self.label.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
+
+        # create hbox layout
+        self.hboxLayout = QHBoxLayout()
+        self.hboxLayout.setContentsMargins(0, 0, 0, 0)
+
+        # set widgets to the hbox layout
+        self.hboxLayout.addWidget(self.playBtn)
+        self.hboxLayout.addWidget(self.slider)
+
+        # create vbox layout
+        self.vboxLayout = QVBoxLayout()
+        self.vboxLayout.addWidget(videowidget)
+        self.vboxLayout.addLayout(self.hboxLayout)
+        self.vboxLayout.addWidget(self.label)
+
+        self.BtnsLayout = QVBoxLayout()
+
+        # OpenBtn
+        self.openBtn = QPushButton('Open Video')
+        self.openBtn.setFixedSize(160, 60)
+        self.openBtn.setMinimumSize(70, 40)
+        self.openBtn.setStyleSheet("QPushButton {background-color: #D3D3D3; color: black; border-radius: 20px;  border: 2px groove gray;font: 10pt 'Source Sans Pro';border-style: circle;}" "QPushButton:pressed {background-color:rgb(119, 136, 153);}")
+        self.playBtn.setIconSize(self.playBtn.minimumSize())
+        self.openBtn.clicked.connect(self.open_file)
+
+        self.hboxLayout = QHBoxLayout()
+        self.hboxLayout.setContentsMargins(0, 0, 0, 0)
+
+        self.BtnsLayout.addWidget(self.openBtn)
+
+        self.openlabel = QLabel()
+
+        self.openlabel.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
+        self.BtnsLayout.addWidget(self.openlabel)
+        self.openlabel.setText('')
+
+        self.openlabel.setStyleSheet('''
+            color: black; 
+            padding-left: 200px;
+        ''')
+
+        # selectFolder
+        self.selectFolder = QPushButton('Select folder')
+        self.selectFolder.setFixedSize(160, 60)
+        self.selectFolder.setMinimumSize(70, 40)
+        self.selectFolder.setStyleSheet(
+            "QPushButton {background-color: #D3D3D3; color: black; border-radius: 20px;  border: 2px groove gray;font: 10pt 'Source Sans Pro';border-style: circle;}" "QPushButton:pressed {background-color:rgb(119, 136, 153);}")
+        self.playBtn.setIconSize(self.playBtn.minimumSize())
+
+        self.selectFolder.clicked.connect(self.select_folder)
+
+        self.BtnsLayout.addWidget(self.selectFolder)
+
+        self.folderlabel = QLabel()
+        self.folderlabel.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
+        self.BtnsLayout.addWidget(self.folderlabel)
+        self.folderlabel.setText('')
+        self.folderlabel.setStyleSheet('''
+                    color: black; 
+                    padding-left: 200px;
+                ''')
+
+        # selectMask
+        self.selectMask = QPushButton('Select mask folder')
+        self.selectMask.setFixedSize(160, 60)
+        self.selectMask.setMinimumSize(70, 40)
+        self.selectMask.setStyleSheet(
+            "QPushButton {background-color: #D3D3D3; color: black; border-radius: 20px;  border: 2px groove gray;font: 9pt 'Source Sans Pro';border-style: circle;}" "QPushButton:pressed {background-color:rgb(119, 136, 153);}")
+        self.playBtn.setIconSize(self.playBtn.minimumSize())
+        self.selectMask.clicked.connect(self.select_mask)
+        self.BtnsLayout.addWidget(self.selectMask)
+
+        self.masklabel = QLabel()
+        self.masklabel.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
+        self.BtnsLayout.addWidget(self.masklabel)
+        self.masklabel.setText('')
+        self.masklabel.setStyleSheet('''
+                    color: black;
+                    padding-left: 200px;
+                ''')
+
+        # create save button
+        self.saveBtn = QPushButton('Save Video as')
+        self.saveBtn.setFixedSize(160, 60)
+        self.saveBtn.setMinimumSize(70, 40)
+        self.saveBtn.setStyleSheet(
+            "QPushButton {background-color: #D3D3D3; color: black; border-radius: 20px;  border: 2px groove gray;font: 10pt 'Source Sans Pro';border-style: circle;}" "QPushButton:pressed {background-color:rgb(119, 136, 153);}" "QLabel { font: bold italic }")
+        self.playBtn.setIconSize(self.playBtn.minimumSize())
+        self.hbox1 = QHBoxLayout()
+        self.hbox1.addStretch(1)
+        self.hbox1.addWidget(self.saveBtn)
+        self.hbox1.addWidget(self.saveBtn)
+        self.saveBtn.clicked.connect(self.save_file)
+        self.BtnsLayout.addWidget(self.saveBtn)
+
+        self.vboxLayout.addLayout(self.BtnsLayout)
+        self.setLayout(self.vboxLayout)
+
+        self.mediaPlayer.setVideoOutput(videowidget)
 
 
-        #create button for playing
+        # media player signals
 
-        icon = QIcon()
-        icon.addPixmap(QPixmap("img_68465.png"))
+        self.mediaPlayer.stateChanged.connect(self.mediastate_changed)
+        self.mediaPlayer.positionChanged.connect(self.position_changed)
+        self.mediaPlayer.durationChanged.connect(self.duration_changed)
 
-        self.playBtn1 = QToolButton()
-        self.playBtn1.setIcon(icon)
-        self.playBtn1.setEnabled(False)
+    def open_file(self):
+        self.filenameOpened, _ = QFileDialog.getOpenFileName(self, "Open Video")
+        if self.filenameOpened != '':
+            self.openlabel.setText(self.filenameOpened)
+            self.folderlabel.setText('')
+            self.filenameFolder = ''
+            self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(self.filenameOpened)))
+            self.playBtn.setEnabled(True)
 
-        self.playBtn1.clicked.connect(lambda: self.play_video(self.mediaPlayer1))
-        self.playBtn2 = QToolButton()
-        self.playBtn2.setEnabled(False)
-        self.playBtn2.setIcon(icon)
-        self.playBtn2.clicked.connect(lambda: self.play_video(self.mediaPlayer2))
+    def save_file(self):
+        self.filenameSaved, _ = QFileDialog.getSaveFileName(self, "Save Video")
 
+        if self.filenameOpened != '' and self.filenameSaved != '' and self.filenameMask != '':
+            # shutil.copy2(self.filenameOpened, self.filenameSaved)
+            # new:
+            ## process_video('/'.join(self.filenameOpened.split('/')[:-1]), self.filenameMask, self.filenameSaved, self.filenameOpened.split('/')[-1])
+            self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(self.filenameSaved)))
+            self.filenameOpened = ''
+            self.filenameFolder = ''
+            self.filenameMask = ''
+            self.filenameSaved = ''
 
+            self.masklabel.setText('')
+            self.openlabel.setText('')
 
-        #create slider
-        self.slider1 = QSlider(Qt.Horizontal)
-        self.slider1.setRange(0, 0)
-        self.slider1.sliderMoved.connect(self.set_position1)
-        self.slider2 = QSlider(Qt.Horizontal)
-        self.slider2.setRange(0, 0)
-        self.slider2.sliderMoved.connect(self.set_position2)
+            self.playBtn.setEnabled(True)
+        elif self.filenameFolder != '' and self.filenameSaved != '' and self.filenameMask != '':
+            # shutil.copy2(self.filenameOpened, self.filenameSaved)
+            # new:
+            ## process_video(self.filenameFolder, self.filenameMask, self.filenameSaved)
+            self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(self.filenameSaved)))
+            self.filenameOpened = ''
+            self.filenameFolder = ''
+            self.filenameMask = ''
+            self.filenameSaved = ''
 
+            self.masklabel.setText('')
+            self.folderlabel.setText('')
 
+            self.playBtn.setEnabled(True)
 
-        #create label
-        self.label1 = QLabel()
-        self.label1.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
-        self.label2 = QLabel()
-        self.label2.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
-        self.filename = ''
-
-        #create hbox layout
-        hboxLayout1 = QHBoxLayout()
-        hboxLayout1.setContentsMargins(0, 0, 0, 0)
-        hboxLayout2 = QHBoxLayout()
-        hboxLayout2.setContentsMargins(0, 0, 0, 0)
-
-        #set widgets to the hbox layout
-        hboxLayout1.addWidget(self.openBtn1)
-        hboxLayout1.addWidget(self.playBtn1)
-        hboxLayout1.addWidget(self.slider1)
-        #hboxLayout2.addWidget(openBtn2)
-        hboxLayout2.addWidget(self.playBtn2)
-        hboxLayout2.addWidget(self.slider2)
-
-
-
-        #create vbox layout
-        vboxLayout1 = QVBoxLayout()
-        vboxLayout1.addWidget(videowidget1)
-        vboxLayout1.addLayout(hboxLayout1)
-        vboxLayout1.addWidget(self.label1)
-        vboxLayout2 = QVBoxLayout()
-        vboxLayout2.addWidget(videowidget2)
-        vboxLayout2.addLayout(hboxLayout2)
-        vboxLayout2.addWidget(self.label2)
-
-        newLayout = QVBoxLayout()
-        newLayout.addLayout(vboxLayout1)
-
-        icon1 = QIcon()
-        icon1.addPixmap(QPixmap("Down-Arrow-Transparent-Image.png"))
-
-        self.bigBtn = QToolButton()
-        self.bigBtn.setIcon(icon1)
-        self.bigBtn.setGeometry(QRect(220, 120, 41, 41))
-        self.bigBtn.setEnabled(False)
-        self.bigBtn.clicked.connect(self.do_smthing_with_videofile)
-
-        newLayout.addWidget(self.bigBtn)
-
-        newLayout.addLayout(vboxLayout2)
-
-        self.setLayout(newLayout)
-
-        self.mediaPlayer1.setVideoOutput(videowidget1)
-        self.mediaPlayer2.setVideoOutput(videowidget2)
-
-
-        #sound
-        hbox = QHBoxLayout()
-        self.slider = QSlider()
-        self.slider.setOrientation(Qt.Horizontal)
-        self.slider.setTickPosition(QSlider.TicksBelow)
-        self.slider.setTickInterval(1)
-        self.slider.setMinimum(0)
-        self.slider.setMaximum(100)
-        self.slider.valueChanged.connect(self.changedValue)
-        self.label = QLabel("0")
-        self.label.setFont(QFont("Sanserif", 15))
-        hbox.addWidget(self.slider)
-        hbox.addWidget(self.label)
-        self.setLayout(hbox)
-        self.slider.show()
-
-
-        self.mediaPlayer1.stateChanged.connect(lambda: self.mediastate_changed(self.mediaPlayer1, self.playBtn1))
-        self.mediaPlayer1.positionChanged.connect(self.position_changed1)
-        self.mediaPlayer1.durationChanged.connect(self.duration_changed1)
-        self.mediaPlayer2.stateChanged.connect(lambda: self.mediastate_changed(self.mediaPlayer2, self.playBtn2))
-        self.mediaPlayer2.positionChanged.connect(self.position_changed2)
-        self.mediaPlayer2.durationChanged.connect(self.duration_changed2)
-
-    def open_file(self, player, btn):
-        self.filename, _ = QFileDialog.getOpenFileName(self, "Open Video")
-
-        if self.filename != '':
-            player.setMedia(QMediaContent(QUrl.fromLocalFile(self.filename)))
-            btn.setEnabled(True)
-            self.bigBtn.setEnabled(True)
-    def play_video(self, player):
-        if player.state() == QMediaPlayer.PlayingState:
-            player.pause()
+    def play_video(self):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.mediaPlayer.pause()
 
         else:
-            player.play()
+            self.mediaPlayer.play()
 
-    def mediastate_changed(self, player, btn):
-        if player.state() == QMediaPlayer.PlayingState:
-            btn.setIcon(
+    def mediastate_changed(self, state):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.playBtn.setIcon(
                 self.style().standardIcon(QStyle.SP_MediaPause)
 
             )
+
         else:
-            btn.setIcon(
+            self.playBtn.setIcon(
                 self.style().standardIcon(QStyle.SP_MediaPlay)
 
             )
 
-    def changedValue(self):
-        size = self.slider.value()
-        self.label.setText(str(size))
+    def position_changed(self, position):
+        self.slider.setValue(position)
 
-    def position_changed1(self, position):
-        self.slider1.setValue(position)
-    def position_changed2(self, position):
-        self.slider2.setValue(position)
+    def duration_changed(self, duration):
+        self.slider.setRange(0, duration)
 
+    def set_position(self, position):
+        self.mediaPlayer.setPosition(position)
 
-    def duration_changed1(self, duration):
-        self.slider1.setRange(0, duration)
-    def duration_changed2(self, duration):
-        self.slider2.setRange(0, duration)
+    def handle_errors(self):
+        self.playBtn.setEnabled(False)
+        self.label.setText("Error: " + self.mediaPlayer.errorString())
 
-    def set_position1(self, position):
-        self.mediaPlayer1.setPosition(position)
-    def set_position2(self, position):
-        self.mediaPlayer2.setPosition(position)
+    def select_folder(self):
+        self.filenameFolder = QFileDialog.getExistingDirectory(self, "Select folder")
+        if self.filenameFolder != '':
+            self.filenameOpened = ''
+            self.openlabel.setText('')
+            self.folderlabel.setText(self.filenameFolder)
+        self.filenameOpened = ''
 
-
-    def handle_errors1(self):
-        self.playBtn1.setEnabled(False)
-        self.label1.setText("Error: " + self.mediaPlayer1.errorString())
-    def handle_errors2(self):
-        self.playBtn2.setEnabled(False)
-        self.label2.setText("Error: " + self.mediaPlayer2.errorString())
-
-    def do_smthing_with_videofile(self):
-        self.mediaPlayer2.setMedia(QMediaContent(QUrl.fromLocalFile(self.filename)))
-        self.playBtn2.setEnabled(True)
-
-
-        volumeDescBtn = QPushButton('V (-)')  # Decrease Volume
-        volumeIncBtn = QPushButton('V (+)')  # Increase Volume
-        volumeDescBtn.clicked.connect(self.decreaseVolume)
-        volumeIncBtn.clicked.connect(self.increaseVolume)
-
-        self.player = QMediaPlayer()
-        self.player.volumeChanged.connect(self.qmp_volumeChanged)
-        self.player.setVolume(60)
-    def increaseVolume(self):
-        vol = self.player.volume()
-        vol = min(vol + 5, 100)
-        self.player.setVolume(vol)
-
-
-    def decreaseVolume(self):
-        vol = self.player.volume()
-        vol = max(vol - 5, 0)
-        self.player.setVolume(vol)
-
-    def qmp_volumeChanged(self):
-        msg = self.statusBar().currentMessage()
-        msg = msg[:-2] + str(self.player.volume())
-        self.statusBar().showMessage(msg)
-
-
-# media player signals
+    def select_mask(self):
+        self.filenameMask = QFileDialog.getExistingDirectory(self, "Select mask")
+        if self.filenameMask != '':
+            self.masklabel.setText(self.filenameMask)
 
 
 app = QApplication(sys.argv)
